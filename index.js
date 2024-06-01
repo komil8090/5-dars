747833c57a3b0fc6a1e3244369cd9dbb2ceba9d4
Mirkomil Mirvohidov <mirkomilmirvohidov4@gmail.com>
2024-06-01 13:39:21 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let a = prompt("Ismingizni kiriting").toLowerCase();
+
+let b = prompt("Harf kiriting").toLowerCase();
+
+if(a.includes(b)){
+    alert("Ismingizba bu harf bor")
+}else{
+    alert("Ismingizba bu harf yoq")
+}
